1d5f972f9faa8811ff451a7d8c68a5877cd49c18
ASHOK MAHARJAN <ashok997@gmail.com>
2019-06-20 17:28:30 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -18,10 +18,12 @@ class Scraper
   end
   
   def get_page
-      
+      doc = Nokogiri::HTML(open("http://learn-co-curriculum.github.io/site-for-scraping/courses"))
   end
   
 end
+
+Scraper.new.get_page
 
 
 
